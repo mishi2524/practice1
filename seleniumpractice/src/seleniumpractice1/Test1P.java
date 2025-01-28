b68d3db209e9bfcad8b8dc36c6d6844c98be34db
USER <USER@DESKTOP-C7LE89C>
2025-01-28 08:00:09 +0530

    update a new file

---
+++ seleniumpractice/src/seleniumpractice1/Test1P.java
@@ -0,0 +1,12 @@
+package seleniumpractice1;
+
+public class Test1P {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello");
+
+	}
+
+}
